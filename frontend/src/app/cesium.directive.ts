73e3ab36a8fb6c3a979ad06be61b96a69e7a7365
yarden <yarden.nitsan@gmail.com>
2026-01-10 10:40:40 +0200

    render problem solved

--- frontend/src/app/cesium.directive.ts
+++ frontend/src/app/cesium.directive.ts
@@ -1,27 +1,19 @@
-import { Directive, ElementRef, AfterViewInit } from '@angular/core';
-import {
-  Viewer,
-  Ion,
-  createWorldTerrainAsync,
-  createOsmBuildingsAsync
-} from 'cesium';
+import { Directive, ElementRef, AfterViewInit, OnDestroy } from '@angular/core';
+import { Viewer, Ion, createWorldTerrainAsync, createOsmBuildingsAsync } from 'cesium';
 import { CesiumManager } from '../modules/cesium-manager-module';
 import { environment } from '../../environment';
 
-@Directive({
-  selector: '[appCesium]'
-})
-export class CesiumDirective implements AfterViewInit {
+@Directive({ selector: '[appCesium]' })
+export class CesiumDirective implements AfterViewInit, OnDestroy {
+  private viewer?: Viewer;
+  private ro?: ResizeObserver;
 
-  constructor(
-    private el: ElementRef,
-    private cesiumManager: CesiumManager
-  ) {}
+  constructor(private el: ElementRef<HTMLElement>, private cesiumManager: CesiumManager) {}
 
   async ngAfterViewInit(): Promise<void> {
     Ion.defaultAccessToken = environment.cesium_public_token;
 
-    const viewer = new Viewer(this.el.nativeElement, {
+    this.viewer = new Viewer(this.el.nativeElement, {
       timeline: true,
       animation: true,
       fullscreenButton: true,
@@ -31,15 +23,27 @@ export class CesiumDirective implements AfterViewInit {
       navigationHelpButton: true
     });
 
-    viewer.terrainProvider = await createWorldTerrainAsync();
-    viewer.scene.primitives.add(await createOsmBuildingsAsync());
-    viewer.scene.globe.depthTestAgainstTerrain = true;
+    this.viewer.terrainProvider = await createWorldTerrainAsync();
+    this.viewer.scene.primitives.add(await createOsmBuildingsAsync());
+    this.viewer.scene.globe.depthTestAgainstTerrain = true;
 
-    this.cesiumManager.setViewer(viewer);
-    
-    setTimeout(() => {
-      viewer.resize();
-      viewer.scene.requestRender();
-    }, 0);
+    this.cesiumManager.setViewer(this.viewer);
+
+    // הכי חשוב: להתאים לכל שינוי גודל אמיתי של הקונטיינר
+    this.ro = new ResizeObserver(() => {
+      if (!this.viewer) return;
+      this.viewer.resize();                 // ייעודי לזה :contentReference[oaicite:7]{index=7}
+      this.viewer.scene.requestRender?.();
+    });
+
+    this.ro.observe(this.el.nativeElement);
+  }
+
+  ngOnDestroy(): void {
+    this.ro?.disconnect();
+    this.ro = undefined;
+
+    this.viewer?.destroy();
+    this.viewer = undefined;
   }
 }
