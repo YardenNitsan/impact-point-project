f7999589fc56440f35e661c65a4113ff5bb26bff
yarden <yarden.nitsan@gmail.com>
2026-03-04 18:38:20 +0200

    environments files will not be included when Im done, for now I need them for faster pulls

---
+++ services/main-service/src/environment.ts
@@ -0,0 +1,6 @@
+export const environmentService = {
+  ALLOWED_ORIGIN: "http://localhost:4200",
+  NODE_PORT: 3000,
+  MONGO_URI: "mongodb://localhost:27017/impact-point",
+  PYTHON_SERVICE_URI: "http://127.0.0.1:8000/simulate-impact",
+};
